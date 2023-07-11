4e2947fc2a458b0c5a04c8a57c239f487599a750
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-11 14:00:29 -0700

    created structured log and added types to arguments

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
+// json url
 var url = "https://jsonplaceholder.typicode.com/todos/1";
+// GET response
 axios_1.default.get(url).then(function (response) {
     var todo = response.data;
+    // pulling json data
     var id = todo.id;
     var title = todo.title;
     var completed = todo.completed;
-    console.log("\n    The Todo with ID: ".concat(id, "\n    Has a title of: ").concat(title, "\n    Is it finished? ").concat(completed, "\n  "));
+    //calling the log with the json data
+    logTodo(id, title, completed);
 });
+// logging out the json data in a structure
+var logTodo = function (id, title, completed) {
+    console.log("\n    The Todo with ID: ".concat(id, "\n    Has a title of: ").concat(title, "\n    Is it finished? ").concat(completed, "\n  "));
+};
--- index.ts
+++ index.ts
@@ -1,23 +1,33 @@
 import axios from "axios";
 
+// json url
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+//includes reference types so that you dont get confused :)
 interface Todo {
   id: number;
   title: string;
   completed: boolean;
 }
 
+// GET response
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
 
+  // pulling json data
   const id = todo.id;
   const title = todo.title;
   const completed = todo.completed;
 
+  //calling the log with the json data
+  logTodo(id, title, completed);
+});
+
+// logging out the json data in a structure
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}
   `);
-});
+};
